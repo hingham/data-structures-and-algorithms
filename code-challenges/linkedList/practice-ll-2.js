b4c3dd18105ff1b1867c52b464097dd98df8bd73
hingham <hannah.c.ingham@gmail.com>
2019-01-09 15:42:07 -0800

    adding warmup

--- code-challenges/linkedList/practice-ll-2.js
+++ code-challenges/linkedList/practice-ll-2.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const list = require('./linked-list.js');
+const queue = require('../stackAndQueues/stacks-and-queues.js');
+const stack = require('../stackAndQueues/stacks-and-queues.js');
 
 const myList = new list.Linkedlist();
 
@@ -19,3 +21,34 @@ function consoleList (ll){
 }
 
 consoleList(myList);
+
+const myQ = new queue.Queue();
+
+const myS = new stack.Stack();
+
+myQ.enqueue('1');
+myQ.enqueue('2');
+myQ.enqueue('3');
+
+function consoleQueue(q){
+  let current = q.front;
+  while(current){
+    console.log(q.dequeue() );
+  }
+}
+
+
+
+myS.push('1');
+myS.push('2');
+myS.push('3');
+
+function consoleStack(s){
+  // let current = s.top;
+  while(s.top){
+    console.log(s.pop());
+  }
+}
+
+consoleStack(myS);
+
